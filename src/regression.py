725677944bbbcff3ab485d3b2c9a2078e189546b
zsiki <siki1958@gmail.com>
2025-06-13 21:14:59 +0200

    fix sphere regression

--- src/regression.py
+++ src/regression.py
@@ -23,11 +23,11 @@ class BaseReg:
 
     def get_dim(self) ->int:
         """ Return dimension of points 2/3 """
-        return self._pnts.shape[0]
+        return self._pnts.shape[1]
 
     def get_n(self) ->int:
         """ Return number of points """
-        return self._pnts.shape[1]
+        return self._pnts.shape[0]
 
     def get_pnts_by_index(self, ind):
         """ Return subset of points """
@@ -88,11 +88,16 @@ class CircleReg(BaseReg):
         """ """
         super().__init__(pnts)
 
-    def lkn_reg(self):
+    def lkn_reg(self, inds=None) ->np.ndarray:
         """ Calculate best fitting circle parameters
         """
-        east = self._pnts[:,0]
-        north = self._pnts[:,1]
+        if inds is None:
+            pnts_act = self._pnts
+        else:
+            pnts_act = self._pnts[inds]
+        pnts_mean = np.mean(pnts_act, axis=0)   # weight point
+        east = pnts_act[:,0] - pnts_mean[0]     # origin to weight point
+        north = pnts_act[:,1] - pnts_mean[1]
         A = np.c_[east, north, np.ones_like(east)]
         # pure term
         b = -(east * east + north * north)
@@ -103,6 +108,8 @@ class CircleReg(BaseReg):
         east_0 = -0.5 * par[0]
         north_0 = -0.5 * par[1]
         r = sqrt(east_0**2 + north_0**2 - par[2])
+        east_0 += pnts_mean[0]   # shift back weight point
+        north_0 += pnts_mean[1]
         self._params = np.array([east_0, north_0, r])
         return self._params
 
@@ -119,6 +126,48 @@ class CircleReg(BaseReg):
     def min_n(self) ->int:
         """ return minimal number of points to define geometry """
         return 3
+
+class SphereReg(BaseReg):
+    """ Calculate best fitting sphere parameters """
+    def __init__(self, pnts:np.ndarray):
+        """ """
+        super().__init__(pnts)
+
+    def lkn_reg(self, inds=None) ->np.ndarray:
+        """
+            calculate best fitting sphere (LSM) on points
+            :returns: x0, y0, z0, R as a numpy array
+        """
+        if inds is None:
+            pnts_act = self._pnts
+        else:
+            pnts_act = self._pnts[inds]
+        pnts_mean = np.mean(pnts_act, axis=0)   # weight point
+        pnts_act -= pnts_mean     # origin to weight point
+        n = pnts_act.shape[0]
+        A = np.c_[pnts_act, np.full(n, 1, 'float64')]
+        b = -np.square(pnts_act[:,0]) - np.square(pnts_act[:,1]) - np.square(pnts_act[:,2])
+        res = np.linalg.lstsq(A, b, rcond=None)[0]
+        self._params = np.array([-0.5 * res[0] + pnts_mean[0],
+                                 -0.5 * res[1] + pnts_mean[1],
+                                 -0.5 * res[2] + pnts_mean[2],
+              sqrt((res[0]**2 + res[1]**2 + res[2]**2) / 4 - res[3])])
+        return self._params
+
+    def dist(self):
+        """Calculate distance from sphere """
+        return np.sqrt((self._pnts[:,0] - self._params[0])**2 +
+                       (self._pnts[:,1] - self._params[1])**2 +
+                       (self._pnts[:,2] - self._params[2])**2) - self._params[3]
+
+    def RMS(self) ->float:
+        """ Calculate mean square error
+        """
+        return sqrt(np.sum(self.dist()**2) / self._pnts.shape[0])
+
+    def min_n(self) ->int:
+        """ return minimal number of points to define geometry """
+        return 4
 
 if __name__ == "__main__":
     east = np.array([1, 5, 3])
--- test/ransac_test.py
+++ test/ransac_test.py
@@ -1,24 +1,84 @@
 from ransac import Ransac
-from regression import LinearReg, CircleReg
+from regression import LinearReg, CircleReg, SphereReg
 import numpy as np
+from matplotlib import pyplot as plt
 
-max_coo = 1000  # coordinate range [..max_coo]
+max_coo = 1000  # coordinate range [0..max_coo]
 n_p = 200       # number of points
+# test for line
+param_0 = np.array([-0.15432, 0.98802, 46.234])
+east = np.random.rand(n_p) * max_coo    # generate random coords
+north = -(param_0[0] * east + param_0[2]) / param_0[1]
+north += np.random.rand(n_p)    # add noise
+en = np.c_[east, north]
+lr = LinearReg(en)
+r = Ransac(lr, 0.5)
+en_line = r.ransac_filter()
+print("-" * 80)
+print("2D line")
+print(f"{en_line.shape[0]} points fit")
+# calculate final params
+lr1 = LinearReg(en_line)
+params = lr1.lkn_reg()
+print(f"Calculated params: {params[0]:.5f} {params[1]:.5f} {params[2]:.3f}")
+print(f"Original   params: {param_0[0]:.5f} {param_0[1]:.5f} {param_0[2]:.3f}")
+print(f"RMS: {lr1.RMS():.3f}")
 # test for plane
-param_0 = np.array([0.57735027, 0.80829038, 0.11547005, 12.45])
-east = np.random.rand(n_p) * max_coo
+param_0 = np.array([0.57735, 0.80829, 0.11547, 12.451])
+east = np.random.rand(n_p) * max_coo    # generate random coords
 north = np.random.rand(n_p) * max_coo
 elev = -(param_0[0] * east + param_0[1] * north + param_0[3]) / param_0[2]
+elev += np.random.rand(n_p) # add random noise
 enz = np.c_[east, north, elev]
-#enz = np.array([[1, -3, 9], [5, 7, -2], [3, 9, 2], [5, 7, 8]])
 lr = LinearReg(enz)
-r = Ransac(lr, 1.0)
+r = Ransac(lr, 0.1)
 enz_plane = r.ransac_filter()
+print("-" * 80)
+print("Plane")
 print(f"{enz_plane.shape[0]} points fit")
 # calculate final params
 lr1 = LinearReg(enz_plane)
 params = lr1.lkn_reg()
-print(f"params: {params}")
-print(f"params: {param_0}")
-# test for line
-
+print(f"Calculated params: {params[0]:.5f} {params[1]:.5f} {params[2]:.5f} {params[3]:.3f}")
+print(f"Original   params: {param_0[0]:.5f} {param_0[1]:.5f} {param_0[2]:.5f} {param_0[3]:.3f}")
+print(f"RMS: {lr1.RMS():.3f}")
+# test for circle
+param_0 = np.array([652.16, 369.25, 35.01])
+alpha = np.random.rand(n_p) * np.pi * 2
+east = param_0[2] * np.sin(alpha) + param_0[0]  # generate random coords
+north = param_0[2] * np.cos(alpha) + param_0[1]
+north += np.random.rand(n_p)    # add random noise
+en = np.c_[east, north]
+cr = CircleReg(en)
+r = Ransac(cr, 0.3)
+en_circle = r.ransac_filter()
+print("-" * 80)
+print("Circle")
+print(f"{en_circle.shape[0]} points fit")
+# calculate final params
+cr1 = CircleReg(en_circle)
+params = cr1.lkn_reg()
+print(f"Calculated params: {params[0]:.3f} {params[1]:.3f} {params[2]:.3f}")
+print(f"Original   params: {param_0[0]:.3f} {param_0[1]:.3f} {param_0[2]:.3f}")
+print(f"RMS: {cr1.RMS():.3f}")
+# test for sphere
+param_0 = np.array([652.161, 369.255, 475.231, 135.012])
+alpha = np.random.rand(n_p) * np.pi * 2
+beta =  np.random.rand(n_p) * np.pi * 2
+east = param_0[3] * np.cos(beta) * np.sin(alpha) + param_0[0]  # generate random coords
+north = param_0[3] * np.cos(beta) * np.cos(alpha) + param_0[1]
+elev = param_0[3] * np.sin(beta) + param_0[2]
+elev += np.random.rand(n_p)    # add random noise
+enz = np.c_[east, north, elev]
+sr = SphereReg(enz)
+r = Ransac(sr, 0.3)
+enz_sphere = r.ransac_filter()
+print("-" * 80)
+print("Sphere")
+print(f"{enz_sphere.shape[0]} points fit")
+# calculate final params
+sr1 = SphereReg(enz_sphere)
+params = sr1.lkn_reg()
+print(f"Calculated params: {params[0]:.3f} {params[1]:.3f} {params[2]:.3f} {params[3]:.3f}")
+print(f"Original   params: {param_0[0]:.3f} {param_0[1]:.3f} {param_0[2]:.3f} {param_0[3]:.3f}")
+print(f"RMS: {sr1.RMS():.3f}")
