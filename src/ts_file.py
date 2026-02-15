c1ed4864ee24e6d0c5969b390e23096d1d422497
zsiki <siki1958@gmail.com>
2026-02-15 19:18:18 +0100

    handler for file decode error

--- src/ts_file.py
+++ src/ts_file.py
@@ -9,20 +9,19 @@ import os
 import re
 from readers import help_table
 
-def guess_file_type(fname: str) -> str:
+def guess_file_type(fname: str, n_lines:int=10, ratio:float=0.6) -> str:
     """ Guess total station file type """
-    N = 10  # the number of lines to check
     lines = []
+    min_lines = int(ratio * n_lines + 0.5)
     if not os.path.isfile(fname):
         return ""   # file does not exists
-    with open(fname) as fp:
-        # read the first N lines
+    with open(fname, encoding="ASCII") as fp:
+        # read the first n_lines lines
         i = 0
-
         for line in fp:
             lines.append(line.strip())
             i += 1
-            if i > N:
+            if i > n_lines:
                 break
     # SDR
     sdr_tag = 0
@@ -32,14 +31,14 @@ def guess_file_type(fname: str) -> str:
             sdr_tag += 1
         if re.match(r"[01][0-9][A-Z]{2}", line):
             sdr_line += 1
-    if sdr_tag and sdr_line > 6:
+    if sdr_tag and sdr_line > min_lines:
         return ".sdr"
     # M5
     m5_line = 0
     for line in lines:
         if re.match(r"For[ _]M5\|Adr", line):
             m5_line += 1
-    if m5_line > 6:
+    if m5_line > min_lines:
         return ".m5"
     # LandXML
     xml_line = 0
@@ -56,21 +55,21 @@ def guess_file_type(fname: str) -> str:
     for line in lines:
         if re.match(r"\*?[41]1[0-9]{4}\+", line):
             gsi_line += 1
-    if gsi_line > 6:
+    if gsi_line > min_lines:
         return ".gsi"
     # GeoEasy
     gez_line = 0
     for line in lines:
         if re.search(r"\{ *5 ", line):
             gez_line += 1
-    if gez_line > 6:
+    if gez_line > min_lines:
         return ".coo"
     # RW5
     rw5_line = 0
     for line in lines:
         if re.match(r"[A-Z]{2},[A-Z]{2}", line):
             rw5_line += 1
-    if rw5_line > 6:
+    if rw5_line > min_lines:
         return ".rw5"
     # Topcon GTS-7
     GTS_TOKENS = ['JOB', 'DATE', 'NAME', 'INST', 'UNITS', 'SCALE', 'ATMOS',
@@ -78,9 +77,9 @@ def guess_file_type(fname: str) -> str:
                   'HD', 'OFFSET', 'PLT_OFF', 'NOTE', 'MLM', 'RES_OBS']
     gts_line = 0
     for line in lines:
-        if re.split('\W+', line)[0] in GTS_TOKENS:
+        if re.split(r'\W+', line)[0] in GTS_TOKENS:
             gts_line += 1
-    if gts_line > 6:
+    if gts_line > min_lines:
         return '.gts7'
     return "unknown"   # unknown file
 
@@ -89,4 +88,7 @@ if __name__ == "__main__":
 
     files = glob.glob("../testdata/*")
     for file in files:
-        print(f"{file} : {help_table.get(guess_file_type(file), 'unknown')}")
+        try:
+            print(f"{file} : {help_table.get(guess_file_type(file), 'unknown')}")
+        except UnicodeDecodeError:
+            print(f"{file} : decode error")
